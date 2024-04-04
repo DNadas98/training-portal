afcd9412e639aea3f2354e6e8e9e05e882d884f0
DNadas98 <daniel.nadas.123@gmail.com>
2024-04-04 10:23:25 +0200

    adjust primary color

--- frontend/src/common/config/colorPaletteConfig.ts
+++ frontend/src/common/config/colorPaletteConfig.ts
@@ -1,20 +1,20 @@
 import {PaletteOptions} from "@mui/material/styles/createPalette";
 
 export const darkPalette: PaletteOptions = {
-  primary: {main: "#1243A6"},
+  primary: {main: "#2d4065"},
   secondary: {main: "#D2EBFF"},
-  background: {default: "#00010D", paper: "#1243A6"},
+  background: {default: "#00010D", paper: "#2d4065"},
   success: {main: "#4CAF50"},
   error: {main: "#F44336"},
   warning: {main: "#FF9800"},
   text: {primary: "#E3F2FD", secondary: "#D2EBFF"}
 };
 export const lightPalette: PaletteOptions = {
-  primary: {main: "#1243A6"},
-  secondary: {main: "#1243A6"},
+  primary: {main: "#2d4065"},
+  secondary: {main: "#2d4065"},
   background: {default: "#E3F2FD", paper: "#ffffff"},
   success: {main: "#81C784"},
   error: {main: "#E57373"},
   warning: {main: "#FFB74D"},
-  text: {primary: "#00010D", secondary: "#05123b"}
+  text: {primary: "#00010D", secondary: "#1a2436"}
 };
