bff00545ccae30a2c2d1f82a633f687dec974721
DNadas98 <daniel.nadas.123@gmail.com>
2024-04-08 00:28:47 +0200

    update colors

--- frontend/src/common/config/colorPaletteConfig.ts
+++ frontend/src/common/config/colorPaletteConfig.ts
@@ -1,17 +1,17 @@
 import {PaletteOptions} from "@mui/material/styles/createPalette";
 
 export const darkPalette: PaletteOptions = {
-  primary: {main: "#2d4065"},
+  primary: {main: "#2D4565FF"},
   secondary: {main: "#D2EBFF"},
-  background: {default: "#00010D", paper: "#2d4065"},
+  background: {default: "#00010D", paper: "#2D4565FF"},
   success: {main: "#4CAF50"},
   error: {main: "#F44336"},
   warning: {main: "#FF9800"},
   text: {primary: "#E3F2FD", secondary: "#D2EBFF"}
 };
 export const lightPalette: PaletteOptions = {
-  primary: {main: "#2d4065"},
-  secondary: {main: "#2d4065"},
+  primary: {main: "#2D4565FF"},
+  secondary: {main: "#2D4565FF"},
   background: {default: "#E3F2FD", paper: "#ffffff"},
   success: {main: "#81C784"},
   error: {main: "#E57373"},
