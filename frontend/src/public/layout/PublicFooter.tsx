5dae7ea754e0b427c24799e3194588dcdb6621f5
Dániel Nádas <daniel.nadas@bucherautomation.hu>
2025-03-24 00:03:30 +0100

    remove outdated info button from site

--- frontend/src/public/layout/PublicFooter.tsx
+++ frontend/src/public/layout/PublicFooter.tsx
@@ -1,18 +1,14 @@
-import {AppBar, Toolbar, Typography} from "@mui/material";
-import MenuSiteInfo from "../../common/utils/components/MenuSiteInfo.tsx";
-import siteConfig from "../../common/config/siteConfig.ts";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import PublicMenu from "../../common/menu/PublicMenu.tsx";
 
 export default function PublicFooter() {
-  const siteName = siteConfig.siteName;
   const currentYear = new Date().getFullYear();
 
   return (
-    <AppBar position="sticky" color="primary" sx={{top: "auto", bottom: 0, marginTop: 4}}>
-      <Toolbar sx={{justifyContent: "center", alignItems: "center"}}>
-        <PublicMenu menuStyle={"small"}/>
-        <MenuSiteInfo/>
-        <Typography>{currentYear}{" "}&copy;{" "}{siteName}</Typography>
+    <AppBar position="sticky" color="primary" sx={{ top: "auto", bottom: 0, marginTop: 4 }}>
+      <Toolbar sx={{ justifyContent: "center", alignItems: "center" }}>
+        <PublicMenu menuStyle={"small"} />
+        <Typography>{currentYear}{" "}&copy;{" "}{window.location.href.split("/")[2]}</Typography>
       </Toolbar>
     </AppBar>
   );
